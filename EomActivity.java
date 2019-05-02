a8d0301d95eaf65f430d4c535766ef743537a8d1
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2019-05-02 17:06:11 +0900

    Add files via upload

---
+++ EomActivity.java
@@ -0,0 +1,14 @@
+package com.example.eye;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class EomActivity  extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_eom);
+        }
+}
