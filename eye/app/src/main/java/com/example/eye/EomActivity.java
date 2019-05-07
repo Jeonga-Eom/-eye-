6fa4bec3f4edfbadddc30bb84c6597231aa63e1d
eom012 <33171438+eom012@users.noreply.github.com>
2019-05-07 19:47:25 +0900

    main activity
    main activity and a part of time_period activity

---
+++ eye/app/src/main/java/com/example/eye/EomActivity.java
@@ -0,0 +1,13 @@
+package com.example.eye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eom);
+    }
+}
