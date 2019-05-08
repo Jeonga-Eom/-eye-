d745feed5c7b57d23f43c8a4608f6cb2fbda1ba7
KIMDAWUN <33215256+KIMDAWUN@users.noreply.github.com>
2019-05-09 00:48:41 +0900

    Update KimActivity.java

--- app/src/main/java/com/example/eye/KimActivity.java
+++ app/src/main/java/com/example/eye/KimActivity.java
@@ -1,7 +1,11 @@
 package com.example.eye;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class KimActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class KimActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kim);
+
+        Button plus = (Button) findViewById(R.id.button_plus);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), KimActivity_plus.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageview1 = findViewById(R.id.HimageView1);
+        imageview1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), KimImage1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
