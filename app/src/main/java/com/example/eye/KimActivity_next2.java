ec3d0b53cad00ac88f1256e2e471d6ec73d8cfe1
KIMDAWUN <33215256+KIMDAWUN@users.noreply.github.com>
2019-05-09 00:48:36 +0900

    Create KimActivity_next2.java

---
+++ app/src/main/java/com/example/eye/KimActivity_next2.java
@@ -0,0 +1,24 @@
+package com.example.eye;
+
+import android.app.Activity;
+import android.renderscript.ScriptGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class KimActivity_next2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kim_next2);
+    }
+}
