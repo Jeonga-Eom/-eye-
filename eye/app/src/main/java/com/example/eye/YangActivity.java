d71f49c35788abadf71d0dabecc380e27b293697
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2019-05-14 23:24:10 +0900

    clear file with package="com.example.eye"

--- eye/app/src/main/java/com/example/eye/YangActivity.java
+++ eye/app/src/main/java/com/example/eye/YangActivity.java
@@ -3,8 +3,6 @@ package com.example.eye;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.hanjin.R;
-
 public class YangActivity extends AppCompatActivity {
 
     @Override
